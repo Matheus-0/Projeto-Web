643bdf35cac11a77e5625089ed2b785451a4e9e0
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2023-11-27 15:30:05 -0300

    Tela inicial.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import { RouterProvider } from "react-router-dom";
+
+import router from "./router";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <RouterProvider router={router} />
   )
 }
---
+++ client/src/pages/home/index.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { MdArrowBack } from "react-icons/md";
+
+const Home = () => {
+  const [shownComponent, setShownComponent] = useState<"login" | "none" | "register">("none");
+
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      {shownComponent === "none" && (
+        <>
+          <h1 className="font-semibold text-3xl">Bem-vindo ao gerenciador de tarefas!</h1>
+
+          <h2 className="text-2xl mt-8">Escolha uma opção:</h2>
+
+          <div className="flex gap-4 mt-8">
+            <button
+              className="bg-blue-500 px-4 py-2 text-white hover:bg-blue-400"
+              onClick={() => setShownComponent("register")}
+            >
+              Criar conta
+            </button>
+
+            <button
+              className="bg-green-600 px-4 py-2 text-white hover:bg-green-500"
+              onClick={() => setShownComponent("login")}
+            >
+              Entrar
+            </button>
+          </div>
+        </>
+      )}
+
+      {shownComponent === "register" && (
+        <div className="bg-slate-100 flex flex-col gap-4 p-8 w-96">
+          <button
+            className="self-start"
+            onClick={() => setShownComponent("none")}
+          >
+            <MdArrowBack className="h-5 w-5" />
+          </button>
+
+          <h2 className="font-semibold text-3xl">Criar conta</h2>
+
+          <input type="text" placeholder="Email" className="outline-none px-3 py-2 w-full" />
+          <input type="password" placeholder="Senha" className="outline-none px-3 py-2 w-full" />
+
+          <button className="bg-blue-500 text-white py-2 px-4 w-full hover:bg-blue-400">Criar conta</button>
+        </div>
+      )}
+
+      {shownComponent === "login" && (
+        <div className="bg-slate-100 flex flex-col gap-4 p-8 w-96">
+          <button
+            className="self-start"
+            onClick={() => setShownComponent("none")}
+          >
+            <MdArrowBack className="h-5 w-5" />
+          </button>
+
+          <h2 className="font-semibold text-3xl">Entrar</h2>
+
+          <input type="text" placeholder="Email" className="outline-none px-3 py-2 w-full" />
+          <input type="password" placeholder="Senha" className="outline-none px-3 py-2 w-full" />
+
+          <button className="bg-green-600 text-white py-2 px-4 w-full hover:bg-green-500">Entrar</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
