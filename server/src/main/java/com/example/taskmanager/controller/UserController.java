040f81c228c87efdd64098369093df9de229cad7
Hércules de Sousa Silva <hercules.silva.ext@synchro.com.br>
2023-12-03 20:22:40 -0300

    Realizando a validação dos dados de registro

--- server/src/main/java/com/example/taskmanager/controller/UserController.java
+++ server/src/main/java/com/example/taskmanager/controller/UserController.java
@@ -2,12 +2,19 @@ package com.example.taskmanager.controller;
 
 import com.example.taskmanager.dto.UserRegistrationDTO;
 import com.example.taskmanager.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/user")
@@ -17,7 +24,15 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody UserRegistrationDTO userRegistrationDTO) {
+    public ResponseEntity<String> registerUser(@RequestBody @Valid UserRegistrationDTO userRegistrationDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errors = bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList();
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erros de validação: " + errors);
+        }
+
         return userService.registerUser(userRegistrationDTO);
     }
 
--- server/src/main/java/com/example/taskmanager/dto/UserRegistrationDTO.java
+++ server/src/main/java/com/example/taskmanager/dto/UserRegistrationDTO.java
@@ -1,11 +1,17 @@
 package com.example.taskmanager.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UserRegistrationDTO {
 
+    @NotNull
+    @Email
     private String email;
+    @NotEmpty
     private String password;
 
 }
